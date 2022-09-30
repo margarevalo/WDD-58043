12230d43eba1c31fa75b40f7d3ac8090956a10d0
margarevalo <89110810+margarevalo@users.noreply.github.com>
2022-10-01 00:27:20 +0800

    Add files via upload

---
+++ Exercise 1.php
@@ -0,0 +1,20 @@
+<html>
+<head><title>Exercise 1</title></head>
+<body>
+<?php
+
+    $FullName = "Margueritte Ann Arevalo";
+    echo $FullName;
+    echo "<br>";
+    $CollegeProgram = "BS Computer Engineering";
+    echo $CollegeProgram;
+    echo "<br>";
+    $YearandSection = "3rd Year - 58043";
+    echo $YearandSection;
+    echo "<br>";
+    echo $string = "Today, I will learn PHP Programming";
+
+?>
+
+</body>
+</html>
