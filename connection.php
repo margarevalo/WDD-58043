b7de76614f0a2675031034e5b45c9a91a09a112b
margarevalo <89110810+margarevalo@users.noreply.github.com>
2022-11-18 09:49:32 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,47 @@
+<?php
+
+
+$server = "localhost";
+$db = "login";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($server,$username,$password,$db);
+/*if($conn)
+	
+{
+	echo "Database is connected";	
+}
+else
+	
+{
+	echo "Database is not connected".mysqli_error($conn);
+}*/
+if(isset($_POST['user']))
+{
+	$username = $_POST['user'];
+	$password = $_POST['text'];
+	
+	$sql = "select * from users where username = '$username' AND password ='$password'";
+	$result = mysqli_query($conn,$sql);
+}
+if(mysqli_num_rows($result)==1)
+{
+	echo "Access Granted\t\t:".$username;
+	exit();
+}
+else
+{
+	echo "Access Denied";
+	exit();
+}
+
+
+
+
+
+
+
+
+
+?>
