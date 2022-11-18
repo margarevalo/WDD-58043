395d3acbe9d9682d5d5640c88f565d965b9b9560
margarevalo <89110810+margarevalo@users.noreply.github.com>
2022-11-18 09:49:17 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>My Login Page</title>
+<body bgcolor="cyan">
+ <form action = "connection.php" method = "post">
+	<p>
+		<label>Username:</label>
+		<input type="text" id = "text" name="user"/>
+	</p>
+	<p>
+		<label>Password:</label>
+		<input type="password" id = "text" name="text"/>
+	</p>
+	<p>
+		<input type="submit" id = "btn" name="Login"/>
+	</p>
+</form>
+</body>
+</head>
+<?php
+
+
+
+
+?>
+</html>
